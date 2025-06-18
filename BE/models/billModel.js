2eda7824744b25e63419e897f0feb9886ba2bdda
Duc-Le-Tri <triduc2k3@gmail.com>
2025-06-18 22:59:07 +0700

    bill

--- BE/models/billModel.js
+++ BE/models/billModel.js
@@ -3,7 +3,7 @@ import { connection } from "../config/connectDb.js";
 const generateMaHoaDon = async () => {
   const sql = "SELECT MaHoaDon FROM hoadon ORDER BY MaHoaDon DESC LIMIT 1";
   const [rows] = await connection.promise().query(sql);
-  if (rows.length === 0) return "HDN001"; 
+  if (rows.length === 0) return "HDN001";
 
   const lastMaHoaDon = rows[0].MaHoaDon;
   const numberPart = parseInt(lastMaHoaDon.replace(/\D/g, ""));
@@ -13,9 +13,10 @@ const generateMaHoaDon = async () => {
 
 const addBillModel = (MaHD, MaHoaDon, tongtien) => {
   return new Promise((resolve, reject) => {
-    const ngaylap = new Date()
-    const sql = "INSERT INTO hoadon(MaHD, MaHoaDon, ngaylap, tongtien) VALUES (?, ?,?,?)";
-    connection.query(sql, [MaHD, MaHoaDon,ngaylap,tongtien], (err) => {
+    const ngaylap = new Date();
+    const sql =
+      "INSERT INTO hoadon(MaHD, MaHoaDon, ngaylap, tongtien) VALUES (?, ?,?,?)";
+    connection.query(sql, [MaHD, MaHoaDon, ngaylap, tongtien], (err) => {
       if (err) return reject(err);
       resolve();
     });
@@ -23,16 +24,15 @@ const addBillModel = (MaHD, MaHoaDon, tongtien) => {
 };
 
 const getAllBillModel = (callback) => {
-  SELECT 
-      `hoadon.MaPhong, 
-      hoadon.TongTien, 
-      hoadon.NgayLap, 
+  SELECT`hoadon.MaPhong, 
+      hoadon.tongtien, 
+      hoadon.ngaylap, 
       hopdong.MaPhong 
     FROM hoadon 
-    JOIN hopdong ON hoadon.MaHD = hopdong.MaHD`
-  connection.query(sql,(err, result) => {
-    if(err) return callback(err);
-    else return callback(null, result)
-  })
-}
-export { addBillModel, generateMaHoaDon , getAllBillModel};
+    JOIN hopdong ON hoadon.MaHD = hopdong.MaHD`;
+  connection.query(sql, (err, result) => {
+    if (err) return callback(err);
+    else return callback(null, result);
+  });
+};
+export { addBillModel, generateMaHoaDon, getAllBillModel };
