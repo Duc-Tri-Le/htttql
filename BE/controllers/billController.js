d862d438e125275a00ed44f38e10a2f60d55a7b4
Duc-Le-Tri <triduc2k3@gmail.com>
2025-06-18 22:57:53 +0700

    bill

--- BE/controllers/billController.js
+++ BE/controllers/billController.js
@@ -1,15 +1,20 @@
-import { addBillModel } from "../models/billModel.js";
+import { escape } from "mysql2";
+import { addBillModel, generateMaHoaDon, getAllBillModel } from "../models/billModel.js";
 import { addBillServiceModel } from "../models/billServiceModel.js";
 import { getAllContractModel } from "../models/contractModel.js";
 
 const createBill = async (req, res) => {
   try {
-    const { MaPhong, infService, MaHoaDon } = req.body;
+    const { MaPhong, infService } = req.body; 
+    let tongtien= 0;
+    for(const service of infService){
+      tongtien += service.DonGia * service.SoLuong
+    }
 
     const allContract = await getAllContractModel(MaPhong);
-
+    const MaHoaDon = await generateMaHoaDon()
     for (const contract of allContract) {
-      await addBillModel(contract.MaHD, MaHoaDon);
+      await addBillModel(contract.MaHD, MaHoaDon, tongtien);
 
       for (const service of infService) {
         await addBillServiceModel(MaHoaDon, service);
@@ -22,4 +27,15 @@ const createBill = async (req, res) => {
   }
 };
 
-export { createBill };
+const getAllBill = async (req, res) => {
+  try {
+    getAllBillModel((err, result) => {
+      if(err) return res.status(500).json({message:"ko lay dc danh sach bill", err:err})
+      else return res.status(200).json({message:"lay thanh cong",result:result})
+    })
+  } catch (error) {
+    return res.status(500).json({ message: "lỗi khi tạo hóa đơn", err });
+  }
+}
+
+export { createBill, getAllBill };
--- BE/models/billModel.js
+++ BE/models/billModel.js
@@ -3,7 +3,7 @@ import { connection } from "../config/connectDb.js";
 const generateMaHoaDon = async () => {
   const sql = "SELECT MaHoaDon FROM hoadon ORDER BY MaHoaDon DESC LIMIT 1";
   const [rows] = await connection.promise().query(sql);
-  if (rows.length === 0) return "HDN001"; // tránh trùng với MaHD
+  if (rows.length === 0) return "HDN001"; 
 
   const lastMaHoaDon = rows[0].MaHoaDon;
   const numberPart = parseInt(lastMaHoaDon.replace(/\D/g, ""));
@@ -11,13 +11,28 @@ const generateMaHoaDon = async () => {
   return "HDN" + newNumber.toString().padStart(3, "0");
 };
 
-const addBillModel = (MaHD, MaHoaDon) => {
+const addBillModel = (MaHD, MaHoaDon, tongtien) => {
   return new Promise((resolve, reject) => {
-    const sql = "INSERT INTO hoadon(MaHD, MaHoaDon) VALUES (?, ?)";
-    connection.query(sql, [MaHD, MaHoaDon], (err) => {
+    const ngaylap = new Date()
+    const sql = "INSERT INTO hoadon(MaHD, MaHoaDon, ngaylap, tongtien) VALUES (?, ?,?,?)";
+    connection.query(sql, [MaHD, MaHoaDon,ngaylap,tongtien], (err) => {
       if (err) return reject(err);
       resolve();
     });
   });
 };
-export { addBillModel, generateMaHoaDon };
+
+const getAllBillModel = (callback) => {
+  SELECT 
+      `hoadon.MaPhong, 
+      hoadon.TongTien, 
+      hoadon.NgayLap, 
+      hopdong.MaPhong 
+    FROM hoadon 
+    JOIN hopdong ON hoadon.MaHD = hopdong.MaHD`
+  connection.query(sql,(err, result) => {
+    if(err) return callback(err);
+    else return callback(null, result)
+  })
+}
+export { addBillModel, generateMaHoaDon , getAllBillModel};
